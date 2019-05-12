42f5c6bc4394447af36211b9fbde257f18f24cdf
inanimatestructure-- <jakehollis425@gmail.com>
2019-05-12 14:33:11 -0300

    changed name to routes

--- routes.js
+++ routes.js
@@ -2,11 +2,16 @@
 var unirest = require('unirest');
 var URLS = new Object();
 var API_HOST = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/";
+var query  = new Array('whatever you want');
 
 URLS.getSummary = "market/get-summary?region=US&lang=en";
 URLS.getMovers = "market/get-movers?region=US&lang=en";
-URLS.getQuotes = "market/get-quotes?region=US&lang=en";
-URLS.getCharts = "market/get-charts?region=US&lang=en";
+URLS.getQuotes = "market/get-quotes?region=US&lang=en&symbols=BAC%2CKC%3DF%2C002210.KS%2CIWM%2CAMECX";
+URLS.getCharts = "market/get-charts?region=US&lang=en&symbol=HYDR.ME&interval=5m&range=1d";
+URLS.getAutoComplete = "market/auto-complete?lang=en&region=en&query=" + query[0];
+URLS.getStockDetail = "stock/get-detail?region=US/lang=en&symbol=NBEV";
+URLS.getStockHistories = "stock/get-histories?region=US&lang=en&symbol=NBEV&from=1231866000&to=1547524844&events=div&interval=1d";
+URLS.getStockNews = "stock/get-news?region=US&category=NBEV";
 
 const readline = require('readline').createInterface({
     input : process.stdin,
@@ -25,9 +30,6 @@ readline.question('What stock market data do you want to see?\n1.Summary\n2.Move
  * IF NEEDED---------------------
  * API-HOST: ----> https://apidojo-yahoo-finance-v1.p.rapidapi.com/
  *  **/
-
-
-
 
 function stringAppend(a,b){
     return Buffer.concat([a,b]);
