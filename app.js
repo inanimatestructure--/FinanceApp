6e10eb9538d7ea94e12b9177b8c30133190c83f6
inanimatestructure-- <jakehollis425@gmail.com>
2019-05-11 22:35:51 -0300

    error fix

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 var unirest = require('unirest');
 
-unirest.post(API_URL){
+unirest.post(API_URL)
     .header("X-RapidAPI-Key", API_KEY)
     .end(function(result){
         console.log(result.status,result.headers,result.body);
     });
-}
