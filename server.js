3011035f5f96e14a06ac2b2f9168c1755514f436
inanimatestructure-- <jakehollis425@gmail.com>
2019-05-12 14:15:17 -0300

    commented out potentially unused routes

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 
 var unirest = require('unirest');
+var URLS = new Object();
+var API_HOST = "https://apidojo-yahoo-finance-v1.p.rapidapi.com/";
+
+URLS.getSummary = "market/get-summary?region=US&lang=en";
+URLS.getMovers = "market/get-movers?region=US&lang=en";
+URLS.getQuotes = "market/get-quotes?region=US&lang=en";
+URLS.getCharts = "market/get-charts?region=US&lang=en";
+
+const readline = require('readline').createInterface({
+    input : process.stdin,
+    output: process.stdout
+})
+
+readline.question('What stock market data do you want to see?\n1.Summary\n2.Movers\n3.Quotes\n4.Charts\n5.', (marketData) => {
+    console.log('${ market }');
+});
+
+ 
 
 /**
  * API KEY: ---->  9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b
@@ -9,46 +27,58 @@ var unirest = require('unirest');
  *  **/
 
 
-unirest.get('https://apidojo-yahoo-finance-v1.p.rapidapi.com')
+
+
+function stringAppend(a,b){
+    return Buffer.concat([a,b]);
+}
+
+unirest.get(stringAppend(API_HOST,MARKET_URLS.getSummary))
+    .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
     .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
     .end(function(result){
         console.log(result.status,result.headers,result.body);
     });
 
-unirest.post('https://apidojo-yahoo-finance-v1.p.rapidapi.com')
-    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
-    .end(function (result) {
-       console.log(result.status, result.headers, result.body);
-    });
+// unirest.post('https://apidojo-yahoo-finance-v1.p.rapidapi.com/')
+//     .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+//     .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+//     .end(function (result) {
+//        console.log(result.status, result.headers, result.body);
+//     });
 
-unirest.put('https://apidojo-yahoo-finance-v1.p.rapidapi.com')
-    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
-    .end(function (result) {
-       console.log(result.status, result.headers, result.body);
-    });
+// unirest.put('https://apidojo-yahoo-finance-v1.p.rapidapi.com/')
+//     .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")    
+//     .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+//     .end(function (result) {
+//        console.log(result.status, result.headers, result.body);
+//     });
 
 
-unirest.delete('https://apidojo-yahoo-finance-v1.p.rapidapi.com')
-    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
-    .end(function (result){
-       console.log(result.status,result.headers,result.body);  
-    });
+// unirest.delete('https://apidojo-yahoo-finance-v1.p.rapidapi.com/')
+//     .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")    
+//     .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+//     .end(function (result){
+//        console.log(result.status,result.headers,result.body);  
+//     });
 
-unirest.patch('https://apidojo-yahoo-finance-v1.p.rapidapi.com')
-  .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
-  .end(function (result) {
-    console.log(result.status, result.headers, result.body);
-  });
+// unirest.patch('https://apidojo-yahoo-finance-v1.p.rapidapi.com/')
+//     .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")   
+//     .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+//     .end(function (result) {
+//         console.log(result.status, result.headers, result.body);
+//     });
    
-/**File uploading **/
-unirest.post('https://apidojo-yahoo-finance-v1.p.rapidapi.com')
-    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
-    .header({'Content-Type': 'multipart/form-data'})
-    .field('parameter', 'value') // Form field
-    .attach('file', '/path/to/file') // Attachment
-    .end(function (result) {
-        console.log(result.status, result.headers, result.body);
-    });
+// /**File uploading **/
+// unirest.post('https://apidojo-yahoo-finance-v1.p.rapidapi.com/')
+//     .header("X-RapidAPI-Host", "apidojo-yahoo-finance-v1.p.rapidapi.com")
+//     .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+//     .header({'Content-Type': 'multipart/form-data'})
+//     .field('parameter', 'value') // Form field
+//     .attach('file', '/path/to/file') // Attachment
+//     .end(function (result) {
+//         console.log(result.status, result.headers, result.body);
+//     });
 
 
 
