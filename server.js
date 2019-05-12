bca8da513faf782907cfe2dab9799b3c6d16104f
inanimatestructure-- <jakehollis425@gmail.com>
2019-05-12 01:39:37 -0300

    skeleton routes

--- server.js
+++ server.js
@@ -7,11 +7,49 @@ var unirest = require('unirest');
  * IF NEEDED---------------------
  * API-HOST: ----> apidojo-yahoo-finance-v1.p.rapidapi.com
  *  **/
-unirest.post(API_URL)
+
+
+unirest.get('')
     .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
     .end(function(result){
         console.log(result.status,result.headers,result.body);
     });
 
+unirest.post('')
+    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+    .end(function (result) {
+       console.log(result.status, result.headers, result.body);
+    });
+
+unirest.put('')
+    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+    .end(function (result) {
+       console.log(result.status, result.headers, result.body);
+    });
+
+
+unirest.delete('')
+    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+    .end(function (result){
+       console.log(result.status,result.headers,result.body);  
+    });
+
    
-unirest.get()
+/**File uploading **/
+unirest.post('')
+    .header("X-RapidAPI-Key", "9a575ef679msh218b48d596f8a39p1edce4jsn9d83480fd74b")
+    .header({'Content-Type': 'multipart/form-data'})
+    .field('parameter', 'value') // Form field
+    .attach('file', '/path/to/file') // Attachment
+    .end(function (result) {
+        console.log(result.status, result.headers, result.body);
+    });
+
+
+
+
+
+
+
+
+
