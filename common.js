5118370351b38a62216561c49524d2db6c8b527a
inanimatestructure-- <jakehollis425@gmail.com>
2019-07-08 00:01:45 -0300

    grab the value from the selector does jack shit

--- common.js
+++ common.js
@@ -3,8 +3,15 @@ $(document).ready(function(){
 });
 
 function init(){
-    var stockFunction = new Object();
     $("#stockFunction").on("change", function(){
+        console.log($(this).val());
+    });
+
+    $("#forexFunction").on("change", function(){
+        console.log($(this).val());
+    });
+
+    $("#cryptocurrencyFunction").on("change", function(){
         console.log($(this).val());
     });
 }
