90692dbb4a68130dbf087ec9140b98f6cdcf80a4
inanimatestructure-- <jakehollis425@gmail.com>
2019-07-13 18:31:20 -0300

    minor change to commonjs

--- common.js
+++ common.js
@@ -3,16 +3,38 @@ $(document).ready(function(){
 });
 
 function init(){
+    var time_series = new Object();
+    var forex = new Object();
+    var cryptocurrency = new Object();
+
+    $.get("https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=BA&apikey=demo",function(data){
+
+        
+        $(".symbolSearchList").on("change",function(){
+            
+            var symbolSearch = $(this).val();
+            
+            time_series.symbol = symbolSearch;
+            forex.symbol = symbolSearch;
+            cryptocurrency.symbol = symbolSearch; 
+
+
+        });
+    });
+
     $("#stockFunction").on("change", function(){
-        console.log($(this).val());
+        time_series.function = $(this).val();
+        console.log(time_series.function);
     });
 
     $("#forexFunction").on("change", function(){
-        console.log($(this).val());
+        forex.function = $(this).val();
+        console.log(forex.function);
     });
 
     $("#cryptocurrencyFunction").on("change", function(){
-        console.log($(this).val());
+        cryptocurrency.function = $(this).val();
+        console.log(cryptocurrency.function);
     });
 } 
 
