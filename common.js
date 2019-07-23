3ec6326a710dc4b58448514369dbe79b0be93397
Jake <jakehollis425@gmail.com>
2019-07-23 15:34:37 -0300

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,15 @@
 var config = "";
 
+const { ipcRenderer } = require('electron');
+
+showName = document.getElementById('showName');
+
+ipcRenderer.on('forWin2', function (event, arg){
+    console.log(arg);
+    showName.innerHTML = arg;
+});
+
+console.log("I'm Window2");
 
 $(document).ready(function(){
     timeseriesScreen();
