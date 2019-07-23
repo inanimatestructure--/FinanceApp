4b414b09e9e0481bf6c78202ffc88810c49221fb
Jake <jakehollis425@gmail.com>
2019-07-23 15:39:45 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-var test = ""; 
+
+const ipcRenderer = require('electron').ipcRenderer
+
+let name = $('#name');
+
+var divHandler = $('#graphDiv').val(); 
+$('#graphDiv').click( function(event){
+    ipcRenderer.send('nameMsg', name.value);
+});
+
+ipcRenderer.on('nameReply', function(event,arg) {
+    console.log(arg) // why/what is not right..
+});
